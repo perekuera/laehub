1de14546feca8661a101a7746b72131a97a04b6e
Pere Bestard <perekuera@gmail.com>
2025-03-15 09:51:00 +0100

    Start project

---
+++ src/main/java/com/laehub/LaehubApplication.java
@@ -0,0 +1,13 @@
+package com.laehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laehub/LaehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
