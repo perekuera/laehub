680a50b3099af60d43a441d0f0d0276606e4f29d
Pere Bestard <perekuera@gmail.com>
2025-03-16 06:54:35 +0100

    configs

--- manage.sh
+++ manage.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+# Create DB
+if [ "$1" = "create" ]; then
+    docker-compose -f ./db/docker-compose.yml down -v
+    docker-compose -f ./db/docker-compose.yml build --no-cache
+    docker-compose -f ./db/docker-compose.yml up -d
+    exit 0
+fi  
+
 # Start DB
 if [ "$1" = "start" ]; then
     docker-compose -f ./db/docker-compose.yml up -d
-    return 0
+    exit 0
 fi
 
 # Stop DB
 if [ "$1" = "stop" ]; then
     docker-compose -f ./db/docker-compose.yml down
-    return 0
+    exit 0
 fi
